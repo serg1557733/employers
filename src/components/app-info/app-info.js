aeb840f2fa6526cfccf7860ff8d3d55c4a8d38ee
serg155alternate <serg1557733@i.ua>
2021-11-08 19:24:47 +0200

    create panels

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import './app-info.css';
+
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников компании</h1>
+            <h2>Общее количество сотрудников</h2>
+            <h2>Премию получат: </h2>
+        </div>
+    );
+}
+
+export default AppInfo;
